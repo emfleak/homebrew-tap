3a104fb7bf6cb5e61e1049c853bbb62f6cf130a8
emfleak <evan.fleak@outlook.com>
2025-09-04 10:51:36 -0400

    Update note formula to v0.2.0

--- Formula/note.rb
+++ Formula/note.rb
@@ -1,10 +1,10 @@
 class Note < Formula
   include Language::Python::Virtualenv
 
-  desc "Simple CLI note-taking tool"
+  desc "Comprehensive CLI note-taking tool"
   homepage "https://github.com/emfleak/note"
-  url "https://github.com/emfleak/note/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "6e85a853af67e2354fbbb2de1dc68c001be0424d65993982aa09cc6c8251638f"
+  url "https://github.com/emfleak/note/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "b2e2ef0fa69750e05f992631d40ebadf7da7da7af7237b501b55223e5e0a450b"
   license "MIT"
 
   depends_on "python@3.12"
